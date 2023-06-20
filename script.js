5fe05b2b40a4d5974d31419e05ffdb5012bec82b
RomaLavrinenko <73121041+RomaLavrinenko@users.noreply.github.com>
2023-06-20 13:26:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// let fib = n => {
+//     let a = 1;
+//     let b = 1;
+//     for (let i = 3; i <= n; i++) {
+//       let c = a + b;
+//       a = b;
+//       b = c;
+//       console.log(b);
+//     }
+    
+// }
+// fib(10);
+
+
+// let start =1;
+// let geometry = function(count,degree)  {
+//     let check=0;
+//     for(let i = 1; i<=count;i++){
+//         start *= degree;
+//         check += start;
+//         console.log(start);
+//     }
+//     return check;
+// }
+// console.log('Return ',geometry(5,2));
+
+
+// let star = +prompt('Write number for start');
+// let fin = +prompt('Write number for finish');
+// let naturalNumb= function(s,n){
+// nextPrime:
+// for (let i = s; i <= n; i++) { 
+
+//   for (let j = 2; j < i; j++) { 
+//     if (i % j == 0) continue nextPrime; 
+//   }
+//   console.log(i);
+// }
+// }
+// naturalNumb(star,fin);
+ 
